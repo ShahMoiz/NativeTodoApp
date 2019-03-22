5b26819a53ffef7d571ef135436546871e9da57e
shahMoiz <abdul_moiz10@hotmail.com>
2019-03-21 20:20:55 -0700

    header Add

---
+++ App.js
@@ -0,0 +1,82 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+// import {Platform, StyleSheet, Text, View} from 'react-native';
+import getTheme from './native-base-theme/components';
+import {StyleProvider, Container, Header, Left, Body, Right, Button, Icon, Title, Text } from 'native-base';
+
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//   android:
+//     'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <ThemeExample/>
+    );
+  }
+}
+class ThemeExample extends Component {
+render() {
+  return (
+          //  <StyleProvider style={getTheme()}>
+
+<Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='arrow-back' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Icon name='search' />
+            </Button>
+            <Button transparent>
+              <Icon name='heart' />
+            </Button>
+            <Button transparent>
+              <Icon name='more' />
+            </Button>
+          </Right>
+        </Header>
+      </Container>
+    //  </StyleProvider>
+
+  )}}
+  //   <View>
+  //   <Text>hello</Text>
+  //   <Text>World</Text>
+  // </View>
+
+  
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
